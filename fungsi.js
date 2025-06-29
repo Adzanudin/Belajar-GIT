5733ae9af1ed44d307b106fc64127dac3822537d
Adzanudin <adzanudinmuhammad68@gmail.com>
2025-06-29 18:38:18 +0700

    tambah fungsi js

--- fungsi.js
+++ fungsi.js
@@ -3,15 +3,13 @@ console.log('Fungsi pertama');
 const add = (a, b) => a + b;
 console.log(add(1,2));
 
-<<<<<<< HEAD
 const substract = (a, b) => a - b;
 console.log(substract(4,2));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
 
 const divide = (a, b) => a / 
 console.log(divide(4, 2));
-=======
-console.log('ini test 2 baru')
->>>>>>> parent of 804f03b (menghapus data yang tidak perlu)
+
+console.log(add(5, 2));
